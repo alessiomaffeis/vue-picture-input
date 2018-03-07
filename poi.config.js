f4798ce245b871693f6259733cacc985da8927c6
Alessio Maffeis <alessio.maffeis@me.com>
2018-03-07 16:23:31 +0100

    Add UMD module for direct browser import

---
+++ poi.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry: './PictureInput.vue',
+  filename: {
+    js: 'vue-picture-input.js'
+  },
+  moduleName: 'PictureInput',
+  extractCSS: false,
+  dist: 'umd',
+  sourceMap: false,
+  html: false,
+  format: 'umd'
+}
