e0af0814033e0b72940d6be41bc8d58a427a7967
Alessio Maffeis <alessio.maffeis@danielwellington.com>
2020-03-04 17:39:31 +0100

    Remove url parameters

--- PictureInput.vue
+++ PictureInput.vue
@@ -528,7 +528,7 @@ export default {
       .then(imageBlob => {
         let e = { target: { files: [] } }
         const fileName = options.fileName || source.split('/').slice(-1)[0]
-        let mediaType = options.mediaType || ('image/' + (options.fileType || fileName.split('.').slice(-1)[0]))
+        let mediaType = options.mediaType || ('image/' + (options.fileType || fileName.split('.').slice(-1)[0].split('?')[0]))
         mediaType = mediaType.replace('jpg', 'jpeg')
         if (mediaType === 'image/svg') {
           mediaType = 'image/svg+xml'
