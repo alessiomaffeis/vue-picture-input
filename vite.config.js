edd4f99fc9aa774b18d0e051577bdf610bef7d06
Alessio Maffeis <alessio.maffeis@me.com>
2025-07-20 20:20:47 +0200

    Update build config

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,22 @@ import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [vue(), cssInjectedByJsPlugin()],
+  plugins: [
+    vue(),
+    ...(process.env.BUILD_FORMAT === "umd" ? [cssInjectedByJsPlugin()] : []),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "PictureInput.vue"),
       name: "PictureInput",
       fileName: (format) => {
         if (format === "es") return "vue-picture-input.es.js";
-        if (format === "cjs") return "vue-picture-input.cjs.js";
         if (format === "umd") return "vue-picture-input.umd.js";
         return `vue-picture-input.${format}.js`;
       },
-      formats: ["es", "cjs", "umd"],
+      formats: process.env.BUILD_FORMAT
+        ? [process.env.BUILD_FORMAT]
+        : ["es", "umd"],
     },
     outDir: "dist",
     cssCodeSplit: false,
